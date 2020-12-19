bb4163ca0ded578f510dfd8b8d94f1c43f5b6cf4
Jan <leski.jn@gmail.com>
2020-12-20 00:38:16 +0100

    progress bar and RNG tamed, updating later stages of the tournament added

--- src/components/MatchSiteComponent.js
+++ src/components/MatchSiteComponent.js
@@ -3,20 +3,36 @@ import { useParams, useHistory } from "react-router-dom";
 
 export const MatchSiteComponent = (props) => {
     const history = useHistory();
+    const [contender1hp, setContender1hp] = useState(100);
+    const [contender2hp, setContender2hp] = useState(100);
 
     const radioRefs = [];
     for (let i = 0; i < props.players.length * 2; i++) {
         radioRefs.push(React.createRef());
     }
 
+    const levels = [];
+    let level = (props.tournamentTree.tree.length + 1) / 2;
+    let j = 0;
+    levels.push(level - 1);
+    while (level > 1) {
+        level /= 2;
+        levels.push(levels[j] + level);
+        j++;
+    }
+
     let { id } = useParams();
     let currentMatch = props.tournamentTree.tree[parseInt(id)];
 
     const goNext = () => {
+        setContender1hp(100);
+        setContender2hp(100);
         history.push("/match/" + String(parseInt(currentMatch.id) + 1));
     };
 
     const goPrev = () => {
+        setContender1hp(100);
+        setContender2hp(100);
         history.push("/match/" + String(parseInt(currentMatch.id) - 1));
     };
 
@@ -24,17 +40,72 @@ export const MatchSiteComponent = (props) => {
         history.push("/tournament");
     };
 
-    const draw = () => {
-        // TODO
-        console.log("dupa");
+    const sleep = async (ms) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    };
+
+    const draw = async (a, b) => {
+        if (a <= 0 && b <= 0) {
+            setContender1hp(100);
+            setContender2hp(100);
+            await sleep(1000).then(async () => {});
+            return await draw(contender1hp, contender2hp);
+        } else if (a <= 0) {
+            currentMatch.winner = currentMatch.contender1;
+            return;
+        } else if (b <= 0) {
+            currentMatch.winner = currentMatch.contender2;
+            return;
+        }
+
+        a -= Math.floor(Math.random() * 10) + 1;
+        b -= Math.floor(Math.random() * 10) + 1;
+
+        setContender1hp(a);
+        setContender2hp(b);
+
+        await sleep(1000).then(async () => {
+            await draw(a, b);
+        });
+    };
+
+    const giveLevel = (id) => {
+        for (let i = 0; i < levels.length; i++) {
+            if (id <= levels[i]) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    const countNextMatchIndes = () => {
+        const treshold = levels[giveLevel(currentMatch.id)];
+        const prev_treshold = levels[giveLevel(currentMatch.id) - 1];
+
+        if (giveLevel(currentMatch.id) === 0) {
+            return treshold + Math.floor(currentMatch.id / 2) + 1;
+        } else {
+            return (
+                treshold +
+                Math.floor((currentMatch.id - prev_treshold - 1) / 2) +
+                1
+            );
+        }
     };
 
     const updateTree = () => {
-        // TODO
-        console.log(currentMatch.winner.name);
+        const next_match_index = countNextMatchIndes();
+
+        if (currentMatch.id % 2 === 0) {
+            props.tournamentTree.tree[next_match_index].contender1 =
+                currentMatch.winner;
+        } else {
+            props.tournamentTree.tree[next_match_index].contender2 =
+                currentMatch.winner;
+        }
     };
 
-    const resolveBattle = () => {
+    const resolveBattle = async () => {
         const playersVotes = [];
         for (let i = 0; i < props.players.length; i++) {
             playersVotes.push(2);
@@ -63,28 +134,34 @@ export const MatchSiteComponent = (props) => {
         currentMatch.contender2score = contender2score;
 
         if (contender1score === contender2score) {
-            draw();
-        } else {
-            if (contender1score > contender2score) {
-                currentMatch.winner = currentMatch.contender1;
-
-                for (let i = 0; i < props.players.length; i++) {
-                    if (playersVotes[i] === 1) {
-                        props.players[i].correctAnswers++;
-                    }
-                }
-            } else {
-                currentMatch.winner = currentMatch.contender2;
-
-                for (let i = 0; i < props.players.length; i++) {
-                    if (playersVotes[i] === 2) {
-                        props.players[i].correctAnswers++;
-                    }
-                }
-            }
-            updateTree();
+            await draw(contender1hp, contender2hp);
         }
 
+        if (
+            contender1score > contender2score ||
+            currentMatch.winner === currentMatch.contender1
+        ) {
+            currentMatch.winner = currentMatch.contender1;
+
+            for (let i = 0; i < props.players.length; i++) {
+                if (playersVotes[i] === 1) {
+                    props.players[i].correctAnswers++;
+                }
+            }
+        } else if (
+            contender1score < contender2score ||
+            currentMatch.winner === currentMatch.contender2
+        ) {
+            currentMatch.winner = currentMatch.contender2;
+
+            for (let i = 0; i < props.players.length; i++) {
+                if (playersVotes[i] === 2) {
+                    props.players[i].correctAnswers++;
+                }
+            }
+        }
+
+        updateTree();
         console.log(props.players);
     };
 
@@ -107,6 +184,8 @@ export const MatchSiteComponent = (props) => {
                                 ></iframe>
                             </p>
                         )}
+                        <progress max="100" value={contender1hp}></progress>
+                        <p>{contender1hp}</p>
                     </div>
                     <div>
                         <p>contender2 :{currentMatch.contender2.name}</p>
@@ -122,6 +201,8 @@ export const MatchSiteComponent = (props) => {
                                 ></iframe>
                             </p>
                         )}
+                        <progress max="100" value={contender2hp}></progress>
+                        <p>{contender2hp}</p>
                     </div>
 
                     {props.players.map((player, index) => (
